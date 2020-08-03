af6e6527d27d0b9d911214eacad8a636386e3735
Andrei Diego Cardoso <andreidiegocardoso@gmail.com>
2020-08-03 12:13:38 -0600

    Initiating the repository.

---
+++ exercises/src/main/java/com/lightbend/futures/CustomerService.java
@@ -0,0 +1,41 @@
+package com.lightbend.futures;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+class CustomerService {
+
+    private CustomerRepository customerRepository;
+
+    CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    CompletableFuture<UUID> addCustomer(String firstName, String lastName, String address, String phoneNumber) {
+        UUID customerId = UUID.randomUUID();
+        return customerRepository.saveCustomer(new Customer(
+                customerId,
+                firstName,
+                lastName,
+                address,
+                phoneNumber
+        )).thenApply(aVoid -> customerId);
+    }
+
+    CompletableFuture<Optional<String>> getCustomerFirstName(UUID customerId) {
+        return customerRepository.getCustomer(customerId).thenApply(optionalCustomer -> optionalCustomer.map(Customer::getFirstName));
+    }
+
+    CompletableFuture<Optional<String>> getCustomerLastName(UUID customerId) {
+        return customerRepository.getCustomer(customerId).thenApply(optionalCustomer -> optionalCustomer.map(Customer::getLastName));
+    }
+
+    CompletableFuture<Optional<String>> getCustomerAddress(UUID customerId) {
+        return customerRepository.getCustomer(customerId).thenApply(optionalCustomer -> optionalCustomer.map(Customer::getAddress));
+    }
+
+    CompletableFuture<Optional<String>> getCustomerPhoneNumber(UUID customerId) {
+        return customerRepository.getCustomer(customerId).thenApply(optionalCustomer -> optionalCustomer.map(Customer::getPhoneNumber));
+    }
+}
